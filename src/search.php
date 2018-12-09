6d4604e7368934571dd4668647c24f4e3f5e4012
DroidKing18 <dkaplan@student.wethinkcode.co.za>
2018-12-09 16:52:07 +0200

    Starting to workon search

--- src/search.php
+++ src/search.php
@@ -1,3 +1,25 @@
 <?php
 
-echo "hi I under construction";
+include ("header.php");
+include ("../config/config.php");
+
+
+$conn = getDB();
+$sql = "SELECT dp, first_name, last_name, login, DOB FROM users";
+foreach ($conn->query($sql) as $user) {
+          $original = $message['messages'];
+}
+if (!isset($_POST['age_low']) && !isset($_POST['age_high']) && !isset($_POST['interest']) && !isset($_POST['loc']) && !isset($_POST['distance']))
+	echo "<form method=POST>
+		<input type=text name=age_low>
+		<input type=text name=age_high><br>
+		<input type=text name=interest>
+		<input type=hidden name=loc>
+		<input type=text name=distance>
+		<input type=submit>";
+else if(checks/validate all data here == true)
+	perform search and return array of all matching users.
+
+?>
+	<div class='grid-item'> <a href='profiles.php?user=<?php echo "hi";//USERNAME//?>'><img height=200 class='photo' id='base64image'
+           src=https://c1.staticflickr.com/5/4851/32333455718_46d068a3d9_b.jpg></a></div>
